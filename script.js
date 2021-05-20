f5f0278485529721d0d920016f0da6ec1ac3d39c
Simran <prabhsimranbajaj@gmail.com>
2021-05-21 02:15:55 +0530

    Local Storage Updated

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 let input = document.querySelector(".task_input");
 let ul = document.querySelector(".task_list");
 
+
+let allTasks = localStorage.getItem("info");
+allTasks = JSON.parse(allTasks);
+if(allTasks != null){
+    for(let i=0;i<allTasks.length;i++){
+        let li = document.createElement("li");
+        li.innerText = allTasks[i].info ;
+        ul.appendChild(li);
+        li.addEventListener("dblclick", deleteTask)
+    }
+}
+    
+
 function deleteTask(e){
+
     e.currentTarget.remove();
+    let info = localStorage.getItem("info");
+    info = JSON.parse(info);
+    info = info.filter(function(data){
+        return (e.currentTarget.innerText != data.info);
+    });
+    localStorage.setItem("info", JSON.stringify(info)); 
 }
 
 function typeEvent(e){
+
     if(e.key == "ArrowUp" || e.key == "ArrowDown"){
         let task = input.value ;
         if(!task){
-            alert(" Added an enpty string !!");
+            alert(" Added an empty string !!");
             return;
         }
         input.value = "";
         let li = document.createElement("li");
+        // let id = uid() ;
         li.innerText = task;
         if(e.key == "ArrowUp"){
             ul.insertBefore(li,ul.firstChild);
@@ -21,7 +43,18 @@ function typeEvent(e){
         else{
             ul.appendChild(li); // adds at the end 
         }
-        li.addEventListener("dblclick", deleteTask);
+
+        let info = localStorage.getItem("info");
+        if(info == null){
+            let data = [{"info": task}];
+            localStorage.setItem("info", JSON.stringify(data));
+        }else{
+            let data = JSON.parse(info);
+            data.push({"info": task});
+            localStorage.setItem("info",JSON.stringify(data));
+        
+            li.addEventListener("dblclick", deleteTask);
+        }
     };
     
 }
